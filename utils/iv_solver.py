a993c4440c25e7a34bfce2ff39df3a375ce5f7a0
Dmitrii <bakaevdima4@gmail.com>
2026-05-15 17:32:24 +0300

    merged call and put prices

--- utils/iv_solver.py
+++ utils/iv_solver.py
@@ -12,7 +12,7 @@ def standard_normal_cdf(x:float) -> float:
 def standard_normal_pdf(x:float) -> float:
     return math.exp(-0.5 * x * x) / math.sqrt(2.0 * math.pi)
 
-def call_price(s:float, k:float, r:float, sigma:float, t:float, type:str):
+def price(s:float, k:float, r:float, sigma:float, t:float, type:str):
     d1, d2 = d_1_d2(s, k, r, sigma, t)
     if type == "CALL":
         return s * standard_normal_cdf(d1) - k * math.exp(-r*t) * standard_normal_cdf(d2)
