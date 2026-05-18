db986dfa75b42008ba37a4efd1755c8bcb67e090
Dmitrii <bakaevdima4@gmail.com>
2026-05-18 15:34:35 +0300

    extracted month number and option type

---
+++ utils/ticker_data.py
@@ -0,0 +1,22 @@
+from tornado.options import options
+
+call_month_arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
+put_month_arr = ["M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]
+
+def extract_data_from_ticker(ticker):
+
+    symbol = ticker[0:2]
+    strike = int(ticker[2:5])
+
+    _ = ticker[6]
+
+    month = ticker[7]
+    year = ticker[8]
+    week = ticker[9] if ticker[9] else 3 #month-option
+
+    if month in call_month_arr:
+        option_type = "CALL"
+        month = call_month_arr.index(month) + 1
+    elif month in put_month_arr:
+        option_type = "PUT"
+        month = put_month_arr.index(month) + 1
