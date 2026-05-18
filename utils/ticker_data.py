934796b8c515982ef02cdcf8ad939335468a63de
Dmitrii <bakaevdima4@gmail.com>
2026-05-18 15:54:05 +0300

    added nth_weekday function

--- utils/ticker_data.py
+++ utils/ticker_data.py
@@ -1,10 +1,18 @@
 from datetime import datetime
+import calendar
 
 call_month_arr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
 put_month_arr = ["M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X"]
 week_arr = ["A", "B", "C", "D"]
 
-def extract_data_from_ticker(ticker):
+
+def nth_weekday(year: int, month: int, weekday: int, n: int):
+    weeks = calendar.monthcalendar(year, month)
+    days = [week[weekday] for week in weeks if week[weekday] != 0]
+
+    return days[n - 1]
+
+def extract_data_from_ticker(ticker:str):
 
     month = ticker[6]
     year = ticker[7]
